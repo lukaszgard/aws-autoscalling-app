1ad743aa5620abb1f8350acdf1826a8ee1eefb32
Łukasz Gardoń <lukaszgardon555@gmail.com>
2021-01-21 08:45:45 +0100

    purge using the ssh key to loginto instances from launch template

--- launch-templete.tf
+++ launch-templete.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "template_01" {
   image_id                             = var.amis[var.aws_region]
   instance_initiated_shutdown_behavior = "terminate"
   instance_type                        = "t2.micro"
-  key_name                             = aws_key_pair.mykeypair.key_name
+  #key_name                             = aws_key_pair.mykeypair.key_name
   vpc_security_group_ids               = [aws_security_group.allow-ssh.id, aws_security_group.allow-tls.id, aws_security_group.allow-http.id]
   tag_specifications {
     resource_type = "instance"
